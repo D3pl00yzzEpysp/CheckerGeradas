d331ded813277c8591ae7f3b50677ed1f04de995
D3pl00yzz Epysp <victorhacking2@gmail.com>
2020-08-11 12:06:20 -0300

    Add files via upload

---
+++ CheckerGeradas.py
@@ -0,0 +1,11 @@
+# Python code obfuscated by www.development-tools.net 
+ 
+
+import base64, codecs
+magic = 'IyAtKi0gY29kaW5nOiB1dGYtOCAtKi0KCmltcG9ydCBqc29uCmltcG9ydCByZXF1ZXN0cwpmcm9tIHRlcm1jb2xvciBpbXBvcnQgY29sb3JlZApmcm9tIHRpbWUgaW1wb3J0IHNsZWVwCgoKCgpwcmludCgiU0VKQSBCRU0gVklORE9cblxuIikKCgp1c3VhcmlvID0gWydlcHlzcCcsJ251bGwnLCdjcmF6eScsJ2RpZGknXQoKcHJpbnQoKQoKbG9naW4gPSBpbnB1dChjb2xv'
+love = 'pzIxXPWZG0qWGvN6VPVfVaWyMPVcXDbXpUWcoaDbXDbXPzyzVTkiM2yhVTyhVUImqJSlnJ86PvNtPvNtpUWcoaDbVxSQEIAGGlOZFHWSHxSRGlVcPvNtPzIfp2H6PvNtPvNtpUWcoaDbVyIGIpBOHxyCVR7Qt08tHxIQG05VEHAWER8vXDbtVNbtVTI4nKDbXDbtVNbwYF0gYF0gYF0gYF0gYF0gYF0gPtcxMJLtM2IlLJEuXTAwXGbXVPNXVPOwLlN9VTAwPtbtVUWypFN9VUWy'
+god = 'cXVlc3RzLnBvc3QoImh0dHBzOi8vd3d3Lm1yY2hlY2tlci5uZXQvY2FyZC9jY24yL2FwaS5waHAiLHsiZGF0YSI6ZiJ7Y2N9In0pCgogIG1lbnNhZ2VtID0gcmVxLnRleHQKICAKICBpZiAiTGl2ZSIgaW4gbWVuc2FnZW06CiAgICAKICAgIHJldHVybiBjb2xvcmVkKGYiTElWRSAtPiB7Y2N9IEBlcHlzcCIsImdyZWVuIikKCiAgZWxzZToKICAgIAoJICByZXR1cm4gY29s'
+destiny = 'o3WyMPuzW0EWEFOCIFOWGyMOGRyRDFNgCvO7L2A9VROypUympPpcPtbXqUW5BtbtVNbtLKWkVQ0to3OyovtaM2phqUu0WljapvpcYaWyLJEfnJ5ypltcPvNtPvOzo3VtoTy2MFOcovOupaR6PvNtVNbtVPOmoTIypPtlXDbtVPNXVPNtpUWcoaDbM2IlLJEuXTkcqzHhpzIjoTSwMFtvKT4vYPVvXFxcPtxWPzI4L2IjqQbXPtyjpzyhqPtaETVtGfBwolOSozAioaElLJEuWlx='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
